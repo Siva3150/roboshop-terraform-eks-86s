119b2c99416ad56f481b086512026022f227c105
Siva prasad <sivaprasad3150@gmail.com>
2025-12-22 15:22:36 +0530

    k8

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -3,7 +3,8 @@ module "eks" {
   version = "~> 21.0" # this is module version
 
   name               = local.common_name_suffix
-  kubernetes_version = "1.33"
+  #kubernetes_version = "1.32"
+  kubernetes_version = var.eks_version
 
   addons = {
     coredns                = {}
@@ -51,8 +52,10 @@ module "eks" {
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
     blue = {
+      create = var.enable_blue
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
+      kubernetes_version = var.eks_nodegroup_blue_version
       instance_types = ["m5.xlarge"]
       iam_role_additional_policies  = {
         amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
@@ -71,32 +74,42 @@ module "eks" {
       #     effect = "NO_SCHEDULE"
       #   }
       # }
+
+        labels = {
+        nodegroup = "blue"
+        }
       
      
     }
 
-    green = {
-      # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
-      ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["m5.xlarge"]
-      iam_role_additional_policies  = {
-        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-      }
+    # green = {
+    #   # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+    #   create = var.enable_green
+    #   ami_type       = "AL2023_x86_64_STANDARD"
+    #   kubernetes_version = var.eks_nodegroup_green_version
+    #   instance_types = ["m5.xlarge"]
+    #   iam_role_additional_policies  = {
+    #     amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+    #     amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    #   }
       
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
+    #   # cluster nodes autoscaling
+    #   min_size     = 2
+    #   max_size     = 10
+    #   desired_size = 2
 
-      taints = {
-        upgrade = {
-          key = "upgrade"
-          value = "true"
-          effect = "NO_SCHEDULE"
-        }
-      }
-  }
+      # taints = {
+      #   upgrade = {
+      #     key = "upgrade"
+      #     value = "true"
+      #     effect = "NO_SCHEDULE"
+      #   }
+      # }
+
+  #       labels = {
+  #       nodegroup = "green"
+  #       }
+  # }
 
   tags = merge(
     local.common_tags,
